eef242ed66cf0c80b964919674368a9fd4f7bb53
Mark Codd <codd82@gmail.com>
2020-05-15 21:03:22 -0700

    images

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,21 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
-#
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#
-# MEDIA_URL = '/media/'
-#
-# MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATIC_ROOT = ''
 
 AWS_ACCESS_KEY_ID = 'AKIA22BNDA6LF4H6OBPP'
 AWS_SECRET_ACCESS_KEY = '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'
