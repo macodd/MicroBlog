3476b89bde02b82c2540bee570d8e347dd5c6424
Mark Codd <codd82@gmail.com>
2020-05-22 11:35:02 -0700

    updated views

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,18 +2,13 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-from .models import UserProfile
-
 from .tasks import register_confirmation_mail
+from .models import UserProfile
 
 
 @receiver(post_save, sender=User)
 def post_save_user_receiver(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        profile = UserProfile(user=instance)
+        profile.save()
         register_confirmation_mail.delay(instance.first_name, instance.email)
-
-
-@receiver(post_save, sender=User)
-def save_user_receiver(sender, instance, created, **kwargs):
-    instance.profile.save()
--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+
 from django.core.mail import EmailMessage
 from render_block import render_block_to_string
 
